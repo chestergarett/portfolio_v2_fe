ccdc53838d28cd4d817f559500b24be609fb7ef4
chestergarett <chestergarett@gmail.com>
2024-11-11 07:43:07 +0800

    grid layout update

--- src/components/dashboards/portfolio/Portfolio.js
+++ src/components/dashboards/portfolio/Portfolio.js
@@ -20,21 +20,7 @@ const Portfolio = () => {
     
     return(
         <div className='p4'>
-            <ResponsiveGridLayout 
-                layout={savedLayout ? JSON.parse(savedLayout) : layout}
-                onLayoutChange={handleLayoutChange}
-                cols={16}
-                // layout={{ lg : savedLayout ? JSON.parse(savedLayout) : layout}}
-                // breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-                // cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
-                rowHeight={100} 
-                isDraggable={editMode}
-                isRearrangeable={editMode}
-                isResizable={editMode}
-                width={1200}
-            >
-                {layout.map(item => <div key={item.i} className={`${editMode ? `resizableIcon` : `` } `}>{item.card}</div>)}
-            </ResponsiveGridLayout>
+            {layout.map(item => <div key={item.i} className={`${editMode ? `resizableIcon` : `` } `}>{item.card}</div>)}
         </div>
         // <div>This is portfolio page</div>
     )
--- src/components/dashboards/resume/Resume.js
+++ src/components/dashboards/resume/Resume.js
@@ -41,7 +41,7 @@ const Resume = () => {
                 // layout={{ lg : savedLayout ? JSON.parse(savedLayout) : layout}}
                 // breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                 // cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
-                rowHeight={100} 
+                rowHeight={100}
                 isDraggable={editMode}
                 isRearrangeable={editMode}
                 isResizable={editMode}
